3f2a2ed5788c0b1dbecb91e147a9a6e9cfae4106
cchunzhe <8038224+cchunzhe@users.noreply.github.com>
2022-08-10 01:04:36 +0800

    Prevent 'undefined' when queue is empty

--- src/commands/music/nowPlaying.js
+++ src/commands/music/nowPlaying.js
@@ -22,9 +22,14 @@ module.exports = {
     }
 
     const distube = client.distube;
-    const queue = distube.getQueue(interaction);
-    const song = queue.songs[0];
+    const queue = distube.getQueue(interaction) || null;
 
+    if (!queue) {
+      InteractionUtil.reply(interaction, Constants.commands.music.QUEUE_EMPTY, InteractionUtil.ReplyType.STRING);
+      return;
+    }
+
+    const song = queue.songs[0];
     const embed = new Discord.EmbedBuilder()
       .setTitle(Constants.commands.music.SONG_NOW_PLAYING)
       .setColor(Constants.misc.embed.COLOR_ACCENT)
