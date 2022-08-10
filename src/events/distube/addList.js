1137114fe5c94b1ffa67d4408c035d0a4fe23547
cchunzhe <8038224+cchunzhe@users.noreply.github.com>
2022-08-10 16:44:16 +0800

    Improved addList event handling

--- src/events/distube/addList.js
+++ src/events/distube/addList.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const InteractionUtil = require('../../util/InteractionUtil.js');
 const Constants = require('../../util/Constants.js');
 
 module.exports = {
@@ -7,28 +6,50 @@ module.exports = {
   async execute(queue, playlist, client) {
     const interaction = playlist.metadata.interaction;
 
-    let title = '▶ Now playing';
-    let description = `[${playlist.name}](${playlist.url})`;
+    const song = queue.songs[0];
+    const embed = new Discord.EmbedBuilder();
+
+    let title = Constants.commands.music.SONG_NOW_PLAYING;
 
     // If queue length > playlist length, means there are previously queued songs
     if (queue.songs.length > playlist.songs.length) {
       title = Constants.commands.music.PLAYLIST_QUEUED;
-      description = `Queued ${playlist.songs.length} items`;
-    }
-
-    const embed = new Discord.EmbedBuilder()
-      .setTitle(title)
-      .setDescription(description)
-      .setColor(Constants.misc.embed.COLOR_ACCENT)
-      .setThumbnail(playlist.thumbnail)
-      .setTimestamp(Date.now())
-      .addFields([
+      embed.setDescription(`\nQueued ${playlist.songs.length} items`);
+    } else {
+      // No songs in queue before this
+      // Show first song in playlist as Now playing
+      embed.addFields([
+        {
+          name: 'Title',
+          value: `[${song.name}](${song.url})`,
+          inline: true,
+        },
         {
           name: 'Duration',
+          value: `${song.formattedDuration}`,
+          inline: true,
+        },
+      ]);
+    }
+
+    embed
+      .setTitle(title)
+      .setColor(Constants.misc.embed.COLOR_ACCENT)
+      .setThumbnail(playlist.thumbnail)
+      .addFields([
+        {
+          name: 'Playlist',
+          value: `[${playlist.name}](${playlist.url})`,
+          inline: false,
+        },
+        {
+          name: 'Playlist duration',
           value: `${playlist.formattedDuration}`,
           inline: false,
         },
       ]);
-    InteractionUtil.editReply(interaction, embed, InteractionUtil.ReplyType.EMBED);
+    await interaction.editReply({
+      embeds: [embed],
+    });
   },
 };
